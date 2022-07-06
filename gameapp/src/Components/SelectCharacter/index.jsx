fe8c16e6670bb17e9a2f1b23138a5c7338ac9571
tallessouza <talles-swd@live.com>
2022-07-06 17:00:10 -0300

    Base project

---
+++ gameapp/src/Components/SelectCharacter/index.jsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from 'react';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import Game from '../../../../artifacts/contracts/Game.sol/Game.json'
+import { ethers } from 'ethers';
+import LoadingIndicator from '../LoadingIndicator';
+import './SelectCharacter.css';
+
+const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+  const [mintingCharacter, setMintingCharacter] = useState(false);
+  
+  useEffect(() => {
+    const { ethereum } = window;
+  
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        Game.abi,
+        signer
+      );
+  
+      setGameContract(gameContract);
+    } else {
+      console.log('Ethereum object not found');
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        console.log('Getting contract characters to mint');
+  
+        const charactersTxn = await gameContract.getAllDefaultCharacters();
+        console.log('charactersTxn:', charactersTxn);
+  
+        const characters = charactersTxn.map((characterData) =>
+          transformCharacterData(characterData)
+        );
+  
+        setCharacters(characters);
+      } catch (error) {
+        console.error('Something went wrong fetching characters:', error);
+      }
+    };
+  
+    /*
+     * Add a callback method that will fire when this event is received
+     */
+    const onCharacterMint = async (sender, tokenId, characterIndex) => {
+      console.log(
+        `CharacterNFTMinted - sender: ${sender} tokenId: ${tokenId.toNumber()} characterIndex: ${characterIndex.toNumber()}`
+      );
+      console.log(`Your NFT is all done -- see it here: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+
+      if (gameContract) {
+        const characterNFT = await gameContract.checkIfUserHasNFT();
+        console.log('CharacterNFT: ', characterNFT);
+        setCharacterNFT(transformCharacterData(characterNFT));
+      }
+    };
+  
+    if (gameContract) {
+      getCharacters();
+      gameContract.on('CharacterNFTMinted', onCharacterMint);
+    }
+  
+    return () => {
+
+      if (gameContract) {
+        gameContract.off('CharacterNFTMinted', onCharacterMint);
+      }
+    };
+  }, [gameContract]);
+
+  const renderCharacters = () =>
+  characters.map((character, index) => (
+    <div className="character-item" key={character.name}>
+      <div className="name-container">
+        <p>{character.name}</p>
+      </div>
+      <img src={character.imageURI} alt={character.name} />
+      <button
+        type="button"
+        className="character-mint-button"
+        onClick={()=> mintCharacterNFTAction(index)}
+      >{`Mint ${character.name}`}</button>
+    </div>
+  ));
+  const mintCharacterNFTAction = async (characterId) => {
+    try {
+      if (gameContract) {
+        setMintingCharacter(true);
+        console.log('Minting character in progress...');
+        const mintTxn = await gameContract.mintCharacterNFT(characterId);
+        await mintTxn.wait();
+        console.log('mintTxn:', mintTxn);
+        setMintingCharacter(false);
+      }
+    } catch (error) {
+      console.warn('MintCharacterAction Error:', error);
+      setMintingCharacter(false);
+    }
+  };
+  return (
+    <div className="select-character-container">
+      <h2>Mint Your Hero. Choose wisely.</h2>
+      {characters.length > 0 && (
+        <div className="character-grid">{renderCharacters()}</div>
+      )}
+      {/* Only show our loading state if mintingCharacter is true */}
+      {mintingCharacter && (
+        <div className="loading">
+          <div className="indicator">
+            <LoadingIndicator />
+            <p>Minting In Progress...</p>
+          </div>
+          <img
+            src="https://media2.giphy.com/media/61tYloUgq1eOk/giphy.gif?cid=ecf05e47dg95zbpabxhmhaksvoy8h526f96k4em0ndvx078s&rid=giphy.gif&ct=g"
+            alt="Minting loading indicator"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SelectCharacter;
---
+++ gameapp/src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xE8281Df0fE22BbFB31ddA8132B6c34Fd284307d4';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
